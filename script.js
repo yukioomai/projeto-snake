afb3640a873fa8307b6e19150f9751a87c5c6a58
yukioomai <yukioomai@gmail.com>
2021-07-28 11:18:38 -0300

    implementacao de game over

--- script.js
+++ script.js
@@ -46,6 +46,13 @@ function iniciarJogo(){
     if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over :()")
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -58,9 +65,16 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+    
 
-let newHead = {
+    let newHead = {
     x: snakeX,
     y: snakeY
 }
